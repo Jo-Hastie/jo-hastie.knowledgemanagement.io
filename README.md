583767ee72bbc2aa114ccdcde3fe00ab79e4c8e5
Joanna Hastie <joannahastie@Joannas-Air-2.home>
2020-03-24 16:45:19 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# KnowledgeManagement
+# KnowledgeManagement
---
+++ index.js
@@ -0,0 +1,87 @@
+/* import the libararies to be used */
+const express = require("express");
+const mongodb = require ("mongodb")
+const cors = require("cors")
+
+/* use apps and set the port */
+const app = express();
+app.use(express.json())
+app.use(cors())
+const port = 3000
+
+/* set up the mongo client */
+const mongoClient = mongodb.MongoClient;
+const mongoUrl = "mongodb://127.0.0.1:27017/"
+
+app.get("/a", (request, response) =>{
+    response.send('test1')
+})
+
+app.get("/", (request, response) => {
+    response.send('Hello world')
+})
+
+/* set up get connection to database */
+app.get("/getThings", (request, response) => {
+
+     mongoClient.connect(mongoUrl, (err, db) => {
+        if(err) throw err;
+        let dbx = db.db("knowledgemanagement")
+
+        const x = dbx.collection("risk").find({}).toArray((err, res) => {
+            if(err) throw err;
+            response.send(res)
+        })
+
+        db.close();
+    })
+
+})
+
+/* set up create connection to database */
+app.post("/makethings", (request, response) => {
+    console.log(request.body.contentUrl)
+    mongoClient.connect(mongoUrl, (err, db) => {
+        if(err) throw err;
+        let dbx = db.db("knowledgemanagement")
+
+        dbx.collection("risk").insertOne(request.body, (err, res) => {
+            if (err) throw err;
+        }
+        )
+
+        db.close();
+    })
+})
+
+
+/*
+{name: "anyname", status: "anystatus" }
+*/
+
+
+/*mongoClient.connect(mongoUrl, (err, db) => {
+    if(err) throw err;
+    let dbx = db.db("knowledgemanagement")
+    /*dbx.createCollection("Y", (err, res) => {
+        if (err) throw err;
+        console.log("Good stuff")
+        db.close();
+    })*/
+    /*
+    let dbz = {name: "anyname", status: "anystatus" };
+    dbx.collection("risk").insertOne(dbz, (err, res) => {
+        if (err) throw err;
+        console.log(res)
+    }
+    )
+    dbx.collection("risk").find({}).toArray((err, res) => {
+        if(err) throw err;
+        console.log(res)
+    })
+    db.close();
+})
+*/
+ /* test to show that the applicaiton is connected to the port */
+app.listen(port, () => console.log("Up and running on port 3000"));
+
