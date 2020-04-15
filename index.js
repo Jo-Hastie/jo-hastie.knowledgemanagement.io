9be23329a9ccecafa3d7f963750eda488fd9d950
Joanna Hastie <joannahastie@Joannas-Air-2.home>
2020-04-15 11:44:42 +0100

    hi

--- index.js
+++ index.js
@@ -17,52 +17,5 @@ let port = process.env.port||3000;
 const mongoClient = mongodb.MongoClient;
 const mongoUrl = "mongodb://127.0.0.1:27017/"
 
-app.get("/a", (request, response) =>{
-    response.send('test1')
-})
-
-app.get("/", (request, response) => {
-    response.send('Hello world')
-})
- 
-//set up get connection to database //
-
-app.get("/getThings", (request, response) => {
-
-     mongoClient.connect(mongoUrl, (err, db) => {
-        if(err) throw err;
-        let dbx = db.db("knowledgemanagement")
-
-        const x = dbx.collection("risk").find({}).toArray((err, res) => {
-            if(err) throw err;
-            response.send(res)
-        })
-
-        db.close();
-    })
-
-})
-
-// set up create connection to database //
-
-app.post("/makethings", (request, response) => {
-    console.log(request.body.contentUrl)
-    mongoClient.connect(mongoUrl, (err, db) => {
-        if(err) throw err;
-        let dbx = db.db("knowledgemanagement")
-
-        dbx.collection("risk").insertOne(request.body, (err, res) => {
-            if (err) throw err;
-        }
-        )
-
-        db.close();
-    })
-})
-
-
-
-
- //test to show that the applicaiton is connected to the port //
 
  app.listen(port, () => console.log("Up and running on port 3000"));
