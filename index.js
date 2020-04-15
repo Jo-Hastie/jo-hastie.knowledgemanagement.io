b9a3534b0d2a882d79dc859528660278185400f9
Joanna Hastie <joannahastie@Joannas-Air-2.home>
2020-04-15 12:11:19 +0100

    connection

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors")
 const app = express();
 app.use(express.json())
 app.use(cors())
-let port = process.env.PORT||3000;
+var port = process.env.PORT||3000;
 
 //mongoose.connect(process.env.MONGODB_URL)//
 
